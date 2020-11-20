29e2f65ff060038e97af9f761dda7a21ed5070b8
Rawis <rawis.n@mail.kmutt.ac.th>
2020-11-20 16:06:08 +0700

    getMenu method

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,10 @@
 public class Menu {
+    public static void getMenu(){
+        System.out.println("----- Menu -----");
+        System.out.println("1: Get Elevator Status");
+        System.out.println("2: Add Person");
+        System.out.println("3: Goto targetFloor");
+        System.out.println("4: Exit Program");
+        System.out.println("-----------------");
+    }
 }
